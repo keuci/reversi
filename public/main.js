c30aed65419dbc682914cb28da8fe2ec43030719
katherine <kecheng@uci.edu>
2018-05-22 03:21:28 -0700

    user name not displaying

--- public/main.js
+++ public/main.js
@@ -2,20 +2,21 @@
 
 /*This function returns the value associated with "whichParam' on the URL*/
 
-function GetURLParameters(whichParam){
-
+function GetURLParameters(whichParam)
+{
     var pageURL = window.location.search.substring(1);
     var pageURLVariables = pageURL.split('&');
     for(var i = 0; i < pageURLVariables.length; i++){
-        var parameterName = pageURLVariables[i].split('=');
-        if (parameterName[0] == whichParam){
-            return parameterName[1];
+      var parameterName = pageURLVariables[i].split('=');
+      if (parameterName[0] == whichParam){
+      return parameterName[1];
         }
     }
   }
 
-var username = getURLParameters('username');
-if ('undefined' == typeof username || !username){
-    username = 'Anonymous_' + Math.random();
+  var username = GetURLParameters('username');
+  if ('undefined' == typeof username || !username){
+      username = 'Anonymous_' + Math.random();
+  }
 
   $('#messages').append('<h4>'+username+'</h4>');
